00d33635f706496a67dd1eb2f65e276a06f95822
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-02-15 10:18:46 -0400

    Implementamos saludar por edad

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 import saludar from "./Saludar";
 
 const nombre_input = document.querySelector("#nombre");
+const edad_input = document.querySelector("#edad");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -8,6 +9,11 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombre_input.value;
+  const edad = Number.parseInt(edad_input.value);
+  if(edad>=30){
+    div.innerHTML = "<p> Hola Sr." + saludar(nombre) + "</p>";
+  }else{
+    div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
+  }
 
-  div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
 });

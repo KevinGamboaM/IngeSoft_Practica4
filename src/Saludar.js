ebb1882c0c4154055eea295f2a45f334e4b3677d
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-02-19 23:21:09 -0400

    Implementamos saludar segun el idioma

--- src/Saludar.js
+++ src/Saludar.js
@@ -2,17 +2,31 @@ function saludar(nombre) {
   
     return nombre;
   }
-export function horario() {
+
+
+export function horario(idioma) {
   
  var hora = new Date().getHours();
  var saludo;
   if (hora >= 1 && hora < 12) {
+    if(idioma == "spanish")
       saludo = "Buenos días";
+    else
+      saludo = "Good morning"
+
   } else if (hora >= 12 && hora < 19) {
-      saludo = "Buenas tardes";
+    if(idioma == "spanish")
+    saludo = "Buenas tardes";
+  else
+    saludo = "Good afternoon"
+
   } else {
-      saludo = "Buenas noches";
+    if(idioma == "spanish")
+    saludo = "Buenas noches";
+  else
+    saludo = "Good night"
   }
   return saludo;
   }
+
   export default saludar ; horario;
--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import saludar, { horario } from "./Saludar";
 const nombre_input = document.querySelector("#nombre");
 const edad_input = document.querySelector("#edad");
 const genero_input = document.querySelector("#genero");
+const idioma_input = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -11,15 +12,36 @@ form.addEventListener("submit", (event) => {
 
   const nombre = nombre_input.value;
   const genero = genero_input.value;
+  const idioma = idioma_input.value;
   const edad = Number.parseInt(edad_input.value);
+
   if(edad>=30 && genero == 'masculino'){
-    div.innerHTML = "<p>"+ horario() + " Sr. " + saludar(nombre) + "</p>";
+    if(idioma == "spanish"){
+      div.innerHTML = "<p>"+ horario(idioma) + " Sr. " + saludar(nombre) + "</p>";
+    }else
+      div.innerHTML = "<p>"+ horario(idioma) + " Mister " + saludar(nombre) + "</p>";
+
+
   }else if(genero == 'masculino'){
-    div.innerHTML = "<p>"+ horario() + "querido amigo " + saludar(nombre) + "</p>";
+    if(idioma == "spanish"){
+      div.innerHTML = "<p>"+ horario(idioma) + " querido amigo " + saludar(nombre) + "</p>";
+    }else
+      div.innerHTML = "<p>"+ horario(idioma) + " dear friend " + saludar(nombre) + "</p>";
+    
+
   }if(edad>=30 && genero == 'femenino'){
-    div.innerHTML = "<p>"+ horario() + " Srta. " + saludar(nombre) + "</p>";
+    if(idioma == "spanish"){
+      div.innerHTML = "<p>"+ horario(idioma) + " Srta. " + saludar(nombre) + "</p>";
+    }else
+      div.innerHTML = "<p>"+ horario(idioma) + " Miss " + saludar(nombre) + "</p>";
+
+    
   }else if(genero == 'femenino'){
-    div.innerHTML = "<p>"+ horario() + " querida amiga " + saludar(nombre) + "</p>";
+    if(idioma == "spanish"){
+      div.innerHTML = "<p>"+ horario(idioma) + " querida amiga " + saludar(nombre) + "</p>";
+    }else
+      div.innerHTML = "<p>"+ horario(idioma) + " dear friend " + saludar(nombre) + "</p>";
+    
   }
 
 

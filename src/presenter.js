c4bba743e8048174e4fb050125172e7956bbcd3e
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-02-19 22:36:14 -0400

    implementamos saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -2,6 +2,7 @@ import saludar from "./Saludar";
 
 const nombre_input = document.querySelector("#nombre");
 const edad_input = document.querySelector("#edad");
+const genero_input = document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -9,11 +10,16 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombre_input.value;
+  const genero = genero_input.value;
   const edad = Number.parseInt(edad_input.value);
-  if(edad>=30){
-    div.innerHTML = "<p> Hola Sr." + saludar(nombre) + "</p>";
-  }else{
-    div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
+  if(edad>=30 && genero == 'masculino'){
+    div.innerHTML = "<p> Hola Sr. " + saludar(nombre) + "</p>";
+  }else if(genero == 'masculino'){
+    div.innerHTML = "<p> Hola querido amigo " + saludar(nombre) + "</p>";
+  }if(edad>=30 && genero == 'femenino'){
+    div.innerHTML = "<p> Hola Srta. " + saludar(nombre) + "</p>";
+  }else if(genero == 'femenino'){
+    div.innerHTML = "<p> Hola querido amiga " + saludar(nombre) + "</p>";
   }
 
 });
